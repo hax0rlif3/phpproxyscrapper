4880f4013a94dfad8930696faa45a6d634296341
Muhammad Hayat <pentesterboy@gmail.com>
2017-04-13 13:47:42 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 #Saves to proxies.txt
 $pattern = "/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?):\d{1,5}/";
-
 $proxysites = array(
 "http://www.example.com/",
 "http://www.example.com/",
@@ -11,7 +10,7 @@ $proxysites = array(
 "http://www.example.com/",
 "http://www.example.com/",
 );
-echo "Proxy scraper developed by Andy Quez\nStarting scraper...\n";
+echo "Proxy scraper\n";
 $proxies = array();
 $start = time();
 foreach ($proxysites as $proxysite)
@@ -21,19 +20,16 @@ foreach ($proxysites as $proxysite)
 		foreach ($fetched as $fetch) {
         $contents = file_get_contents($fetch[1]);
         preg_match_all($pattern, $contents, $matches);
-
         foreach ($matches[0] as $match)
         {
                 $proxies[] = $match;
         }
 	}
 }
-
 $proxies = array_unique($proxies);
 $count = count($proxies);
-
 $fh = fopen('proxies.txt', 'w');
 fwrite($fh, implode("\n", $proxies));
 fclose($fh);
-
 echo "Succesfully scraped $count proxies in ".(time() - $start)." seconds!\n";
+?>
